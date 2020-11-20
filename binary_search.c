97d2e96748f92bf812f9a624199424925857c29a
el10686 <vnapostolop@gmail.com>
2020-11-20 14:44:13 +0200

    Add binary_search

--- binary_search.c
+++ binary_search.c
@@ -1,52 +1,41 @@
 #include <stdio.h>
 
-int binarysearch(int A[], int key, int left, int right)
-{
+int binarysearch(int A[], int key, int left, int right) {
 
-    if(left <= right)
-    {
+	if(left <= right) {
 
-        int mid;
+		int mid;
 
-        mid = (left + right) / 2;
+		mid = (left + right) / 2;
 
-        if (key == A[mid])
-        {
-            return key;
-        }
+		if (key == A[mid]) {
+			return mid;
+		}
 
-        if (key < A[mid])
-        {
-            binarysearch(A, key, left, mid - 1);
-        }
-        else
-        {
-            binarysearch(A, key, mid + 1, right);
-        }
-    }
+		if (key < A[mid]) {
+			return binarysearch(A, key, left, mid - 1);
+		}
+		else {
+			return binarysearch(A, key, mid + 1, right);
+		}
+	}
 
-    return -1;
+	return -1;
 }
 
-int main()
-{
+int main() {
 
-    int i, n, key, ret;
+	int i, n, key, ret;
+	scanf("%d", &n);
+	int A[n];
 
-    scanf("%d", &n);
+	for(i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
 
-    int A[n];
+	scanf("%d", &key);
+	ret = binarysearch(A, key, 0, n - 1);
+	printf("%d\n", ret);
 
-    for(i = 0; i < n; i++)
-    {
-        scanf("%d", &A[i]);
-    }
-
-    scanf("%d", &key);
-
-    ret = binarysearch(A, key, 0, n - 1);
-
-    printf("%d ", ret);
-
-    return 0;
+	return 0;
 }
